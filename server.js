634b50b158f126f377228bc1fc8066e556f2c8b6
hitallocavas <hcs3@cin.ufpe.br>
2019-02-15 00:37:55 -0300

    fix express

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');  
 const app = express();  
-app.use(express.static(__dirname + '/dist'));  
+app.use(express.static(__dirname + '/dist/aida-plataforma'));  
 app.all('*', (req, res) => {  
   res.status(200).sendFile(__dirname + '/dist/aida-plataforma/index.html');  
 });  
